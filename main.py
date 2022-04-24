8a8b19863df06074468fb91ef1d6fadb87b888f2
Varchasva Khare <72777353+varchasvkhare@users.noreply.github.com>
2022-04-24 14:40:58 +0530

    prefix customization

--- main.py
+++ main.py
@@ -1,1 +1,49 @@
 import discord
+from discord.ext import commands
+import random
+import json
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+def get_prefix(client, message):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    return prefixes[str(message.guild.id)]
+
+bot = commands.AutoShardedBot(shard_count=1, command_prefix=(get_prefix), intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+    await bot.change_presence(activity=discord.Activity(name=(f'd/help | {str(len(bot.guilds))} servers'), type=discord.ActivityType.playing))
+
+@bot.event
+async def on_guild_join(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = 'd/'
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+@bot.event
+async def on_guild_remove(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id))
+
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
+
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined in {member.joined_at}')
+
+bot.run('ODcxNjk3MTgwMDQwMjUzNDgx.YQfFQw.vJLVpVsQKuKv8OsfMQdDVzqbcqs')
