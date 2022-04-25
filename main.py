232533c10fcdbcf220451bfab80afe4d2af86002
Varchasva Khare <72777353+varchasvkhare@users.noreply.github.com>
2022-04-25 12:30:21 +0530

    e

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import discord
 from discord.ext import commands, tasks
 import import_expression
 
-async def get_prefix(client, message):
+async def get_prefix(bot: commands.Bot, message: discord.Message):
     with open('prefixes.json', 'r') as f:
         prefixes = json.load(f)
     return prefixes[str(message.guild.id)]
